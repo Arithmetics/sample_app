9d663ff634b1ff585016b38400d4dc0a7eb00345
Brock <brock.m.tillotson@gmail.com>
2017-09-11 15:44:35 -0700

    finished login/logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  #helper method for the test. same as the one for the controlller
+  #but tests cant use those so this one is made (just with a different name)
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
