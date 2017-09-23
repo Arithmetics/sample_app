31e206220814aea4d75311f01390e144e42cd7e9
Brock <brock.m.tillotson@gmail.com>
2017-09-22 20:52:00 -0700

    trying carrier aws again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,11 @@ if Rails.env.production?
     config.aws_bucket =  ENV['S3_BUCKET']      # required
     config.aws_acl    =  :public_read
 
+
     config.aws_credentials = {
       access_key_id:      ENV['S3_KEY'],       # required
-      secret_access_key:  ENV['S3_SECRET']     # required
+      secret_access_key:  ENV['S3_SECRET']
+      region: ENV['S3_REGION']
     }
   end
 end
